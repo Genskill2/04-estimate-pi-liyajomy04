10f8f192a6f8b23d85841ec2145fd3c31880a026
liyajomy04 <57555095+liyajomy04@users.noreply.github.com>
2021-06-01 11:41:45 +0530

    wallis.c

--- wallis.c
+++ wallis.c
@@ -1,26 +1,38 @@
-#include <assert.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
+#include <cs50.h>
 
-float wallis_pi(int);
+int main()
+{
+  int n[2800 + 1];
+  int i, k;
+  int b, d;
+  int c = 0;
 
-int main(void) {
-  float pi;
-  for (int i=0; i<5; i++) {
-    pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) > 0.15)) {
-      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
-      abort();
+    for (i = 0; i < 2800; i++)
+    {
+        n[i] = 2000;
     }
-  }
 
-  for (int i=500; i<3000; i++) {
-    pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) < 0.01)) {
-      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
-      abort();
+    for (k = 2800; k > 0; k -= 14)
+    {
+        d = 0;
+
+        i = k;
+        for (;;)
+        {
+            d += n[i] * 10000;
+            b = 2 * i - 1;
+
+            n[i] = d % b;
+            d /= b;
+            i--;
+            if (i == 0) break;
+            d *= i;
+        }
+        printf("%.4d", c + d / 10000);
+        c = d % 10000;
     }
-  }
+
+    return 0;
 }
 
